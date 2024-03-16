ad82cd18667c0a235a002c10b43e7612113bb2ae
James Rhodes <james.rhodes@hotmail.com.au>
2024-03-16 15:26:43 +1100

    Added reset command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{f32::consts::PI, time::Duration};
 
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle, window::RawHandleWrapper};
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 
 // Colors
 const PARTICLE_AMPLITUDE: f32 = 50.;
@@ -22,7 +22,7 @@ const RECEIVER_DELTA_X_PER_SECOND: f32 = 2. * RECEIVER_WIDTH / RECEIVER_TIME_SCA
 const RECEIVER_PLOT_COLOR: Color = Color::BLACK;
 const RECEIVER_PLOT_RADIUS: f32 = 10.;
 const RECEIVER_PLOT_SIZE: Vec3 = Vec2::splat(RECEIVER_PLOT_RADIUS).extend(1.0);
-const RECEIVER_SPEED: f32 = 80.;
+const RECEIVER_SPEED: f32 = 100.;
 
 const PARTICLE_SPAWN_RATE_MS: u64 = 50;
 
@@ -60,6 +60,7 @@ fn main() {
                 destroy_particle,
                 handle_rx_collision,
                 move_rx,
+                reset_simulation,
             )
                 .chain(),
         )
@@ -67,12 +68,159 @@ fn main() {
 }
 
 fn setup(
+    meshes: ResMut<Assets<Mesh>>,
+    materials: ResMut<Assets<ColorMaterial>>,
+    mut commands: Commands,
+) {
+    commands.spawn(Camera2dBundle::default());
+    start_simulation(meshes, materials, commands);
+}
+
+fn propagate_particle(mut query: Query<(&mut Transform, &SignalParticle)>, time: Res<Time>) {
+    for (mut particle_transforms, signal_particle) in query.iter_mut() {
+        let t = time.elapsed().as_millis() as f32 / 1000.;
+
+        let a = -signal_particle.amplitude;
+        let k = 2. * PI * signal_particle.frequency / signal_particle.speed; // v = \omega/k =
+                                                                             // \lambda/T = \lambda * f
+        let x = particle_transforms.translation.x;
+        particle_transforms.translation.x += signal_particle.speed * time.delta_seconds();
+
+        // Classic
+        // propagating wave equation
+        let f = signal_particle.frequency;
+        particle_transforms.translation.y = a * f32::sin(k * x - 2. * PI * f * t);
+    }
+}
+
+fn produce_particle(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut query: Query<(Entity, &mut Transmitter)>,
+    time: Res<Time>,
+) {
+    for (tx_entity, mut tx) in query.iter_mut() {
+        tx.spawn_rate.tick(time.delta());
+
+        if tx.spawn_rate.finished() {
+            let new_particle = commands
+                .spawn((
+                    MaterialMesh2dBundle {
+                        mesh: meshes.add(Circle::default()).into(),
+                        material: materials.add(PARTICLE_COLOR),
+                        transform: Transform::from_translation(tx.spawn_point.extend(-1.))
+                            .with_scale(PARTICLE_SIZE),
+                        ..default()
+                    },
+                    SignalParticle {
+                        amplitude: PARTICLE_AMPLITUDE,
+                        speed: PARTICLE_SPEED,
+                        frequency: PARTICLE_FREQUENCY,
+                    },
+                ))
+                .id();
+
+            commands.entity(tx_entity).add_child(new_particle);
+        }
+    }
+}
+
+fn handle_rx_collision(
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut commands: Commands,
+    sig_query: Query<(&Parent, Entity, &GlobalTransform, &Transform), With<SignalParticle>>,
+    mut rx_query: Query<(Entity, &Transform, &mut Receiver)>,
+    time: Res<Time>,
+) {
+    for (sig_parent, sig_entity, sig_global_transform, sig_transform) in sig_query.iter() {
+        let particle_pos = sig_global_transform.translation().xy();
+        for (rx_entity, rx_transform, mut rx) in rx_query.iter_mut() {
+            let rx_translation = rx_transform.translation;
+            let rx_right_bound = rx_translation.x + RECEIVER_WIDTH;
+            let rx_top_bound = rx_translation.y + RECEIVER_HEIGHT / 2.;
+            let rx_bottom_bound = rx_translation.y - RECEIVER_HEIGHT / 2.;
+
+            if particle_pos.y < rx_top_bound
+                && particle_pos.y > rx_bottom_bound
+                && particle_pos.x < rx_right_bound
+            {
+                let t = time.elapsed().as_millis() as f32 / 1000.;
+                let y = sig_transform.translation.y;
+                commands
+                    .entity(sig_parent.get())
+                    .remove_children(&[sig_entity]);
+                commands.entity(sig_entity).despawn();
+
+                if rx.current_draw_position > 2. * RECEIVER_WIDTH {
+                    // If we have already plotted over the entire width of the receiver then just
+                    // don't do anything
+                    commands.entity(rx_entity).remove::<Mover>();
+                    continue;
+                }
+
+                let plot_point = commands
+                    .spawn(MaterialMesh2dBundle {
+                        mesh: meshes.add(Circle::default()).into(),
+                        material: materials.add(RECEIVER_PLOT_COLOR),
+                        transform: Transform::from_xyz(
+                            (RECEIVER_WIDTH) - rx.current_draw_position,
+                            y,
+                            2.,
+                        )
+                        .with_scale(RECEIVER_PLOT_SIZE),
+                        ..default()
+                    })
+                    .id();
+
+                commands.entity(rx_entity).add_child(plot_point);
+
+                if rx.prev_collision_time.is_none() {
+                    rx.prev_collision_time = Some(t);
+                }
+                rx.current_draw_position +=
+                    RECEIVER_DELTA_X_PER_SECOND * (t - rx.prev_collision_time.unwrap());
+
+                rx.prev_collision_time = Some(t);
+            }
+        }
+    }
+}
+
+fn destroy_particle(
+    mut commands: Commands,
+    camera: Query<(&Camera, &GlobalTransform)>,
+    sig_query: Query<(&Parent, Entity, &GlobalTransform), With<SignalParticle>>,
+) {
+    let (camera, camera_transform) = camera.single();
+    for (parent, entity, transform) in sig_query.iter() {
+        let particle_pos = transform.translation();
+
+        let world_left_bound = camera
+            .viewport_to_world(camera_transform, Vec2::new(0., 0.))
+            .unwrap()
+            .origin
+            .x;
+
+        if particle_pos.x < world_left_bound - PARTICLE_RADIUS {
+            commands.entity(parent.get()).remove_children(&[entity]);
+            commands.entity(entity).despawn();
+        }
+    }
+}
+
+fn move_rx(mut rx_query: Query<&mut Transform, (With<Receiver>, With<Mover>)>, time: Res<Time>) {
+    for mut transform in rx_query.iter_mut() {
+        transform.translation.x += RECEIVER_SPEED * time.delta_seconds();
+    }
+}
+
+fn start_simulation(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
     mut commands: Commands,
 ) {
-    commands.spawn(Camera2dBundle::default());
-
     let transmitter_x = 400.;
     let transmitter_y = 200.;
     let half_tri_size = TRANSMITTER_SIZE / 2.;
@@ -134,138 +282,23 @@ fn setup(
     ));
 }
 
-fn propagate_particle(mut query: Query<(&mut Transform, &SignalParticle)>, time: Res<Time>) {
-    for (mut particle_transforms, signal_particle) in query.iter_mut() {
-        let t = time.elapsed().as_millis() as f32 / 1000.;
-
-        let a = -signal_particle.amplitude;
-        let k = 2. * PI * signal_particle.frequency / signal_particle.speed; // v = \omega/k =
-                                                                             // \lambda/T = \lambda * f
-        let x = particle_transforms.translation.x;
-        particle_transforms.translation.x += signal_particle.speed * time.delta_seconds();
-
-        // Classic
-        // propagating wave equation
-        let f = signal_particle.frequency;
-        particle_transforms.translation.y = a * f32::sin(k * x - 2. * PI * f * t);
-    }
-}
-
-fn produce_particle(
+fn reset_simulation(
+    meshes: ResMut<Assets<Mesh>>,
+    materials: ResMut<Assets<ColorMaterial>>,
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    mut query: Query<(Entity, &mut Transmitter)>,
-    time: Res<Time>,
+    input: Res<ButtonInput<KeyCode>>,
+    tx_query: Query<Entity, With<Transmitter>>,
+    rx_query: Query<Entity, With<Receiver>>,
 ) {
-    for (tx_entity, mut tx) in query.iter_mut() {
-        tx.spawn_rate.tick(time.delta());
-
-        if tx.spawn_rate.finished() {
-            let new_particle = commands
-                .spawn((
-                    MaterialMesh2dBundle {
-                        mesh: meshes.add(Circle::default()).into(),
-                        material: materials.add(PARTICLE_COLOR),
-                        transform: Transform::from_translation(tx.spawn_point.extend(-1.))
-                            .with_scale(PARTICLE_SIZE),
-                        ..default()
-                    },
-                    SignalParticle {
-                        amplitude: PARTICLE_AMPLITUDE,
-                        speed: PARTICLE_SPEED,
-                        frequency: PARTICLE_FREQUENCY,
-                    },
-                ))
-                .id();
-
-            commands.entity(tx_entity).add_child(new_particle);
+    if input.pressed(KeyCode::KeyR) {
+        for tx in tx_query.iter() {
+            commands.entity(tx).despawn_recursive();
         }
-    }
-}
 
-fn handle_rx_collision(
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    mut commands: Commands,
-    sig_query: Query<(Entity, &GlobalTransform, &Transform), With<SignalParticle>>,
-    mut rx_query: Query<(Entity, &Transform, &mut Receiver)>,
-    time: Res<Time>,
-) {
-    for (sig_entity, sig_global_transform, sig_transform) in sig_query.iter() {
-        let particle_pos = sig_global_transform.translation().xy();
-        for (rx_entity, rx_transform, mut rx) in rx_query.iter_mut() {
-            let rx_translation = rx_transform.translation;
-            let rx_right_bound = rx_translation.x + RECEIVER_WIDTH;
-            let rx_top_bound = rx_translation.y + RECEIVER_HEIGHT / 2.;
-            let rx_bottom_bound = rx_translation.y - RECEIVER_HEIGHT / 2.;
-
-            if particle_pos.y < rx_top_bound
-                && particle_pos.y > rx_bottom_bound
-                && particle_pos.x < rx_right_bound
-            {
-                let t = time.elapsed().as_millis() as f32 / 1000.;
-                let y = sig_transform.translation.y;
-                commands.entity(sig_entity).despawn();
-
-                if rx.current_draw_position > 2. * RECEIVER_WIDTH {
-                    // If we have already plotted over the entire width of the receiver then just
-                    // don't do anything
-                    commands.entity(rx_entity).remove::<Mover>();
-                    continue;
-                }
-
-                let plot_point = commands
-                    .spawn(MaterialMesh2dBundle {
-                        mesh: meshes.add(Circle::default()).into(),
-                        material: materials.add(RECEIVER_PLOT_COLOR),
-                        transform: Transform::from_xyz(
-                            (RECEIVER_WIDTH) - rx.current_draw_position,
-                            y,
-                            2.,
-                        )
-                        .with_scale(RECEIVER_PLOT_SIZE),
-                        ..default()
-                    })
-                    .id();
-
-                commands.entity(rx_entity).add_child(plot_point);
-
-                if rx.prev_collision_time.is_none() {
-                    rx.prev_collision_time = Some(t);
-                }
-                rx.current_draw_position +=
-                    RECEIVER_DELTA_X_PER_SECOND * (t - rx.prev_collision_time.unwrap());
-
-                rx.prev_collision_time = Some(t);
-            }
+        for rx in rx_query.iter() {
+            commands.entity(rx).despawn_recursive();
         }
-    }
-}
 
-fn destroy_particle(
-    mut commands: Commands,
-    camera: Query<(&Camera, &GlobalTransform)>,
-    sig_query: Query<(Entity, &GlobalTransform), With<SignalParticle>>,
-) {
-    let (camera, camera_transform) = camera.single();
-    for (entity, transform) in sig_query.iter() {
-        let particle_pos = transform.translation();
-
-        let world_left_bound = camera
-            .viewport_to_world(camera_transform, Vec2::new(0., 0.))
-            .unwrap()
-            .origin
-            .x;
-
-        if particle_pos.x < world_left_bound - PARTICLE_RADIUS {
-            commands.entity(entity).despawn();
-        }
-    }
-}
-
-fn move_rx(mut rx_query: Query<&mut Transform, (With<Receiver>, With<Mover>)>, time: Res<Time>) {
-    for mut transform in rx_query.iter_mut() {
-        transform.translation.x += RECEIVER_SPEED * time.delta_seconds();
+        start_simulation(meshes, materials, commands);
     }
 }
