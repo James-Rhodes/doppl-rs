0a70a2472dbf7016d264b78b73a7564726c26f46
James Rhodes <james.rhodes@hotmail.com.au>
2024-03-24 14:41:53 +1100

    Made resizing nice

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 use std::{f32::consts::PI, time::Duration};
 
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle, transform::TransformSystem};
+use bevy::{
+    prelude::*,
+    render::{
+        camera::RenderTarget,
+        render_resource::{
+            Extent3d, TextureDescriptor, TextureDimension, TextureFormat, TextureUsages,
+        },
+        view::RenderLayers,
+    },
+    sprite::MaterialMesh2dBundle,
+    transform::TransformSystem,
+    window::WindowResized,
+};
 
 // Colors
 const PARTICLE_AMPLITUDE: f32 = 50.;
@@ -26,6 +38,25 @@ const RECEIVER_SPEED: f32 = 100.;
 
 const PARTICLE_SPAWN_RATE_MS: u64 = 10;
 
+/// In-game resolution width.
+const RES_WIDTH: u32 = 1280;
+// const RES_WIDTH: u32 = 600;
+
+/// In-game resolution height.
+const RES_HEIGHT: u32 = 720;
+// const RES_HEIGHT: u32 = 600;
+
+/// Default render layers for pixel-perfect rendering.
+/// You can skip adding this component, as this is the default.
+const PIXEL_PERFECT_LAYERS: RenderLayers = RenderLayers::layer(0);
+
+/// Render layers for high-resolution rendering.
+const HIGH_RES_LAYERS: RenderLayers = RenderLayers::layer(1);
+
+#[derive(Resource)]
+struct ResetTimer {
+    timer: Timer,
+}
 #[derive(Component, Default)]
 struct Receiver {
     prev_collision_time: Option<f32>,
@@ -54,10 +85,36 @@ struct SignalParticle {
     frequency: f32,
 }
 
+/// Camera that renders the pixel-perfect world to the [`Canvas`].
+#[derive(Component)]
+struct InGameCamera;
+
+/// Camera that renders the [`Canvas`] (and other graphics on [`HIGH_RES_LAYERS`]) to the screen.
+#[derive(Component)]
+struct OuterCamera;
+
+/// Low-resolution texture that contains the pixel-perfect world.
+/// Canvas itself is rendered to the high-resolution world.
+#[derive(Component)]
+struct Canvas;
+
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup)
+    let mut app = App::new();
+
+    if cfg!(feature = "webdev") {
+        app.add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                canvas: Some("#doppl-rs".into()),
+                ..default()
+            }),
+            ..default()
+        }));
+    } else {
+        // app.add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()));
+        app.add_plugins(DefaultPlugins);
+    }
+    app.add_systems(Startup, (setup, setup_camera))
+        // .insert_resource(Msaa::Off)
         .add_systems(
             Update,
             (
@@ -65,13 +122,15 @@ fn main() {
                 produce_particle,
                 move_rx,
                 reset_simulation,
+                reset_simulation_timer,
+                fit_canvas,
             )
                 .chain(),
         )
         .add_systems(
             PostUpdate,
-            (handle_rx_collision, destroy_particle).after(TransformSystem::TransformPropagate), // Need
-                                                                                                // to wait til bevy propagates the transform before using the global transform
+            (handle_rx_collision).after(TransformSystem::TransformPropagate), // Need
+                                                                              // to wait til bevy propagates the transform before using the global transform
         )
         .run();
 }
@@ -81,7 +140,7 @@ fn setup(
     materials: ResMut<Assets<ColorMaterial>>,
     mut commands: Commands,
 ) {
-    commands.spawn(
+    commands.spawn((
         TextBundle::from_section(
             "Press 'r' to restart the simulation",
             TextStyle {
@@ -95,39 +154,72 @@ fn setup(
             left: Val::Px(15.),
             ..default()
         }),
-    );
-    commands.spawn(
-        TextBundle::from_section(
-            "Transmitter",
-            TextStyle {
-                font_size: 20.,
-                ..default()
-            },
-        )
-        .with_style(Style {
-            position_type: PositionType::Absolute,
-            top: Val::Px(150.0),
-            right: Val::Px(40.),
-            ..default()
-        }),
-    );
-    commands.spawn(
-        TextBundle::from_section(
-            "Receiver",
-            TextStyle {
-                font_size: 20.,
-                ..default()
-            },
-        )
-        .with_style(Style {
-            position_type: PositionType::Absolute,
-            top: Val::Px(150.0),
-            left: Val::Px(130.),
-            ..default()
-        }),
-    );
-    commands.spawn(Camera2dBundle::default());
+        HIGH_RES_LAYERS,
+    ));
+
+    commands.insert_resource(ResetTimer {
+        timer: Timer::new(Duration::from_secs(10), TimerMode::Repeating),
+    });
     start_simulation(meshes, materials, commands);
+}
+
+fn setup_camera(mut commands: Commands, mut images: ResMut<Assets<Image>>) {
+    let canvas_size = Extent3d {
+        width: RES_WIDTH,
+        height: RES_HEIGHT,
+        ..default()
+    };
+
+    // this Image serves as a canvas representing the low-resolution game screen
+    let mut canvas = Image {
+        texture_descriptor: TextureDescriptor {
+            label: None,
+            size: canvas_size,
+            dimension: TextureDimension::D2,
+            format: TextureFormat::Bgra8UnormSrgb,
+            mip_level_count: 1,
+            sample_count: 1,
+            usage: TextureUsages::TEXTURE_BINDING
+                | TextureUsages::COPY_DST
+                | TextureUsages::RENDER_ATTACHMENT,
+            view_formats: &[],
+        },
+        ..default()
+    };
+
+    // fill image.data with zeroes
+    canvas.resize(canvas_size);
+
+    let image_handle = images.add(canvas);
+
+    // this camera renders whatever is on `PIXEL_PERFECT_LAYERS` to the canvas
+    commands.spawn((
+        Camera2dBundle {
+            camera: Camera {
+                // render before the "main pass" camera
+                order: -1,
+                target: RenderTarget::Image(image_handle.clone()),
+                ..default()
+            },
+            ..default()
+        },
+        InGameCamera,
+        PIXEL_PERFECT_LAYERS,
+    ));
+
+    // spawn the canvas
+    commands.spawn((
+        SpriteBundle {
+            texture: image_handle,
+            ..default()
+        },
+        Canvas,
+        HIGH_RES_LAYERS,
+    ));
+
+    // the "outer" camera renders whatever is on `HIGH_RES_LAYERS` to the screen.
+    // here, the canvas and one of the sample sprites will be rendered by this camera
+    commands.spawn((Camera2dBundle::default(), OuterCamera, HIGH_RES_LAYERS));
 }
 
 fn propagate_particle(mut query: Query<(&mut Transform, &SignalParticle)>, time: Res<Time>) {
@@ -172,6 +264,7 @@ fn produce_particle(
                         speed: PARTICLE_SPEED,
                         frequency: PARTICLE_FREQUENCY,
                     },
+                    PIXEL_PERFECT_LAYERS,
                 ))
                 .id();
 
@@ -238,28 +331,6 @@ fn handle_rx_collision(
 
                 rx.prev_collision_time = Some(t);
             }
-        }
-    }
-}
-
-fn destroy_particle(
-    mut commands: Commands,
-    camera: Query<(&Camera, &GlobalTransform)>,
-    sig_query: Query<(&Parent, Entity, &GlobalTransform), With<SignalParticle>>,
-) {
-    let (camera, camera_transform) = camera.single();
-    for (parent, entity, transform) in sig_query.iter() {
-        let particle_pos = transform.translation();
-
-        let world_left_bound = camera
-            .viewport_to_world(camera_transform, Vec2::new(0., 0.))
-            .unwrap()
-            .origin
-            .x;
-
-        if particle_pos.x < world_left_bound - PARTICLE_RADIUS {
-            commands.entity(parent.get()).remove_children(&[entity]);
-            commands.entity(entity).despawn();
         }
     }
 }
@@ -338,6 +409,7 @@ fn create_simulation(
             transform: Transform::from_xyz(transmitter_x, y_pos, 1.),
             ..default()
         },
+        PIXEL_PERFECT_LAYERS,
     ));
 
     let mb = MaterialMesh2dBundle {
@@ -347,9 +419,20 @@ fn create_simulation(
         ..default()
     };
     match movement {
-        Movement::Left => commands.spawn((mb, Receiver::default(), Mover(Movement::Left))),
-        Movement::Right => commands.spawn((mb, Receiver::default(), Mover(Movement::Right))),
-        Movement::Stationary => commands.spawn((mb, Receiver::default())),
+        Movement::Left => commands.spawn((
+            mb,
+            Receiver::default(),
+            Mover(Movement::Left),
+            PIXEL_PERFECT_LAYERS,
+        )),
+
+        Movement::Right => commands.spawn((
+            mb,
+            Receiver::default(),
+            Mover(Movement::Right),
+            PIXEL_PERFECT_LAYERS,
+        )),
+        Movement::Stationary => commands.spawn((mb, Receiver::default(), PIXEL_PERFECT_LAYERS)),
     };
 }
 
@@ -357,11 +440,13 @@ fn reset_simulation(
     meshes: ResMut<Assets<Mesh>>,
     materials: ResMut<Assets<ColorMaterial>>,
     mut commands: Commands,
+    mut reset_timer: ResMut<ResetTimer>,
     input: Res<ButtonInput<KeyCode>>,
     tx_query: Query<Entity, With<Transmitter>>,
     rx_query: Query<Entity, With<Receiver>>,
 ) {
     if input.pressed(KeyCode::KeyR) {
+        reset_timer.timer.reset();
         for tx in tx_query.iter() {
             commands.entity(tx).despawn_recursive();
         }
@@ -371,5 +456,41 @@ fn reset_simulation(
         }
 
         start_simulation(meshes, materials, commands);
+    }
+}
+
+fn reset_simulation_timer(
+    meshes: ResMut<Assets<Mesh>>,
+    materials: ResMut<Assets<ColorMaterial>>,
+    mut commands: Commands,
+    mut reset_timer: ResMut<ResetTimer>,
+    time: Res<Time>,
+    tx_query: Query<Entity, With<Transmitter>>,
+    rx_query: Query<Entity, With<Receiver>>,
+) {
+    reset_timer.timer.tick(time.delta());
+    if reset_timer.timer.finished() {
+        for tx in tx_query.iter() {
+            commands.entity(tx).despawn_recursive();
+        }
+
+        for rx in rx_query.iter() {
+            commands.entity(rx).despawn_recursive();
+        }
+
+        start_simulation(meshes, materials, commands);
+    }
+}
+
+/// Scales camera projection to fit the window (integer multiples only).
+fn fit_canvas(
+    mut resize_events: EventReader<WindowResized>,
+    mut projections: Query<&mut OrthographicProjection, With<OuterCamera>>,
+) {
+    for event in resize_events.read() {
+        let h_scale = event.width / RES_WIDTH as f32;
+        let v_scale = event.height / RES_HEIGHT as f32;
+        let mut projection = projections.single_mut();
+        projection.scale = 1. / h_scale.min(v_scale);
     }
 }
